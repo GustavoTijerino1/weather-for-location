8a1d71bba188a3b33583d3c9841380b89d15c037
Gustavo Tijerino <busg26@gmail.com>
2022-03-17 08:48:48 -0500

    Modified HTML, CSS , JS.

--- script.js
+++ script.js
@@ -1,29 +1,72 @@
-const cityName = document.getElementById('cityName')
-const srchHistory = document.getElementById('srchHistory')
+// − 273.15) × 9/5 + 32 temp
+//  get items from the objects
+
+let sbmtBttn = document.getElementById('sbmt')
+var apiKey= 'aba392ccda7fe400f92ed7fab594a456';
+
+//  'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={apiKey}'
 
 
-function handleFormSubmit(e) {
-    e.preventDefault();
+function getApi() {
   
-    // select form element by its `name` attribute and get its value
-    var pickCity = document.querySelector('input[name=pick]').value
-  
-    // if there's nothing in the form entered, don't print to the page
-    if (!pickCity) {
-      console.log('No shopping item filled out in form!');
-      return;
-    }
+  // fetch request gets a list of all the repos for the node.js organization
+  var requestUrl =
+  `https://api.openweathermap.org/data/2.5/weather?q=${pickCity.value}&appid=${apiKey}`;
+console.log(pickCity.value);
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+      
+ 
+      
+    });
+}
 
-    const hstrList = document.createElement('li')
+
+
+
+var hstrList = document.createElement('li');
+
+var searchedCitys= []
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  
+  var pickCity = document.getElementsByName('pick')[0].value
+  getApi();
+  // select form element by its `name` attribute and get its value
+  // if there's nothing in the form entered, don't print to the page
+  // if (!pickCity) {
+    //   console.log('No City filled out in form!');
+    //   return;
+    // }
     
-  
-    // print to the page
-    // se.append('<li>' + pickCity + '</li>');
-  
-    // clear the form input element
-    $('input[name="shopping-input"]').val('');
+    const srchHistory = document.getElementById('srchHistory')
+
+    hstrList.textContent= pickCity;
+   
+    srchHistory.appendChild(hstrList);
+
+    searchedCitys.push(pickCity);
+
+    localStorage.setItem("potato", JSON.stringify(searchedCitys) || []);
+    
+    
+    grabMyList()
+    
   }
   
-  // Create a submit event listener on the form element
-  shoppingFormEl.on('submit', handleFormSubmit);
+ sbmtBttn.addEventListener('click', handleFormSubmit)
+
+  function grabMyList(event) {
+    // event.preventDefault()
+
+    var getHistory = JSON.parse(localStorage.getItem("potato"));
+    console.log(getHistory);
+    hstrList.textContent = getHistory;
+   
+  }
   
