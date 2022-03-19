ad231945641a9d013566f86a924d3a037bccc592
Gustavo Tijerino <busg26@gmail.com>
2022-03-19 04:46:40 -0500

    Updated JS and Read ME

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # weather-for-location
+
+## Task
+I created a Weather app , by using HTML, CSS and JS. I used various functions and global variables to make the app functional. I also used EventListeners to activate some functions by a click of a button. I grabbed all the information of the city's weather from the API.
+
+I used some of my old work and these websites for more information:  
+
+https://www.w3schools.com/
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+https://stackoverflow.com/
+
+
+ I also had help from my classmates and my Tutor Zachary Auerbach. 
+
+ ## Final Product
+
+![Screenshot of my final page.](weather.png)
+
+## Links
+
+[Github pages](https://github.com/GustavoTijerino1/weather-for-location)
+
+[Official Website](https://gustavotijerino1.github.io/weather-for-location/)
--- script.js
+++ script.js
@@ -1,67 +1,104 @@
-// − 273.15) × 9/5 + 32 temp
-//  get items from the objects
-
-
-
+var date = document.getElementById('time')
 var hstrList = document.createElement('li');
-
-var searchedCitys= []
+var cityInArr= []
 let sbmtBttn = document.getElementById('sbmt')
 var apiKey= 'aba392ccda7fe400f92ed7fab594a456';
+let cast = document.getElementById('forecast')
 
-//  'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={apiKey}'
+date.innerText = moment().format('l');
 
 
 function getApi() {
   
   var requestUrl =
   `https://api.openweathermap.org/data/2.5/weather?q=${pickCity.value}&units=imperial&appid=${apiKey}`;
-console.log(pickCity.value);
+  console.log(pickCity.value);
   fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      // const uvI = data.uvi;
-      // console.log(uvI);
-      const {name} = data;
-      const {icon, description} = data.weather[0];
-      const {temp} = data.main;
-      const {humidity} = data.main;
-      const {speed} = data.wind;
-      // console.log(name,icon, description,temp, humidity,speed);
-      
-      // console.log(newTemp);
-      
-      document.querySelector('#currentCity').textContent = name;
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    // const uvI = data.uvi;
+    // console.log(uvI);
+    let {name} = data;
+    let {icon, description} = data.weather[0];
+    let {temp} = data.main;
+    let {humidity} = data.main;
+    let {speed} = data.wind;
+    
+    
+    document.querySelector('#currentCity').textContent = name;
       document.querySelector('.icon').src="https://openweathermap.org/img/wn/"+ icon + ".png"
       document.querySelector('.description').textContent.toUpperCase = description;
       document.querySelector('.temp').textContent= temp;
       document.querySelector('.humidity').textContent = humidity;
       document.querySelector('.wind').textContent = speed;
-
       
-      // data.day.forEach(element => {
+      console.log(data.coord.lon)
+      console.log(data.coord.lat)
+
+      // let lon = data.coord.lon
+      // let lat = data.coord.lat
+      // let secondApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey
+      
+      // fetch(queryURL2)
+      // .then(function (response) {
+      //     return response.json();
+      //   })
+      //   .then(function (data) {
+      //       console.log(data)
+      
+      for(i=1; i<6; i++){
         
-      // });
+          let timeDisplay =  moment().add(i, 'days').format('l');;
+
+            var dayCard = moment().add(i, 'days').format('l');  //gives us next 5 days
+            console.log(dayCard)
+           
+            let card = document.createElement('div')
+            card.classList.add( 'card','cardDiv')
+
+
+            var dateCard = document.createElement('h3') 
+            dateCard.textContent = timeDisplay;
+           
+
+            let cardDetail =document.createElement('div')
+            cardDetail.classList.add('cardStyle')  
+
+            let unorderL = document.createElement('ul')
+
+            let li1 = document.createElement('li')
+            li1.textContent= `Temp: ${temp}   ℉`
+            li1.classList.add('li-update')
+            
+            let li2 = document.createElement('li')
+            li2.textContent= `Humidity: ${humidity}%`
+            li2.classList.add('li-update')
+
+            let li3 = document.createElement('li')
+            li3.textContent= `Wind Speed: ${speed} MPH`
+            li3.classList.add('li-update')
+
+
+            unorderL.appendChild(li1)
+            unorderL.appendChild(li2)
+            unorderL.appendChild(li3)
+            card.appendChild(unorderL)
+            card.appendChild(dateCard)
+            card.appendChild(cardDetail)
+            cast.appendChild(card)
+         }
+
+    
+          
+        });
       
-    });
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      
+    };
+  
+  
 
 
 
@@ -81,22 +118,14 @@ sbmtBttn.addEventListener('click', function handleFormSubmit(event) {
    
     srchHistory.appendChild(hstrList);
 
-    searchedCitys.push(pickCity);
+    cityInArr.push(pickCity);
 
-    localStorage.setItem("potato", JSON.stringify(searchedCitys) || []);
+    localStorage.setItem("potato", JSON.stringify(cityInArr) || []);
     
-    
-    grabMyList()
+    var getHistory = JSON.parse(localStorage.getItem("potato"));
+    hstrList.textContent = getHistory;
     
   });
   
   
-  function grabMyList(event) {
-    // event.preventDefault()
-
-    var getHistory = JSON.parse(localStorage.getItem("potato"));
-    console.log(getHistory);
-    hstrList.textContent = getHistory;
-   
-  }
   
